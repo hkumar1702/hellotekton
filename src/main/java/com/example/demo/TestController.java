219e6902acc9ab2836bf3f26dec97f4bca891fa3
hkumar1702 <46435924+hkumar1702@users.noreply.github.com>
2021-06-29 20:36:47 +0530

    Update TestController.java

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -13,7 +13,7 @@ public class TestController {
 	@GetMapping("/hellotekton")
 	public String testMessage() {
 		LOGGER.info("hellotekton ::");
-		return "My First Tekton POC-5";
+		return "My First Tekton POC-6";
 
 	}
 
